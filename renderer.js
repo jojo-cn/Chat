2075cde9802e575adaf120b4e873c71b31655b21
jojo-cn <634759311@qq.com>
2018-04-28 09:20:40 +0800

    1

--- renderer.js
+++ renderer.js
@@ -1,3 +1,8 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+var btn = document.getElementById('send')
+var txt = document.getElementById('chat_send_content')
+btn.onclick = function(){
+	txt.value = txt.value + '123'
+}
